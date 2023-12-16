4f0f3536239e674957ad70e8d618134e3ea7b7d2
Temesgen dora <tdemeke36@gmail.com>
2023-12-15 21:50:34 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,10 +7,10 @@
 void bubble_sort(int *array, size_t size)
 {
     int temp = array[0];
-
-    for (int i = 0; i < size; i++)
+    int i, j;
+    for (i = 0; i < size; i++)
     {
-        for (int j = 0; j < size - 1; j++)
+        for (j = 0; j < size - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
