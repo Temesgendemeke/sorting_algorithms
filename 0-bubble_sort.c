20bba9bd6dd45b218cad8871d2b3685952619f05
Temesgen dora <tdemeke36@gmail.com>
2023-12-15 21:36:44 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts array in ascending order
+ * array - to be sorted
+ * size - length of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int temp = array[0];
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+        }
+    }
+
+
+}
+}
