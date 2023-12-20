d5faf75fb4bb33c8119b80d87a2635b8920dcbfa
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-12-19 19:05:13 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,50 +6,59 @@
  */
 void quick_sort(int *array, size_t size)
 {
-        quick_req(array, 0, size -1);
-        
-        
+	if (array == NULL || size < 2)
+		return;
+	quick_req(array, 0, size - 1, size);
 }
 
-void quick_req(int *array, int low, int high)
+void quick_req(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-    int part = partition(array, low, high);
+	int part;
 
-    quick_req(array, low, part - 1);
-    quick_req(array,part + 1, high);
-    
-    }
-    
+	if (low < high)
+	{
+		part = partition_arr(array, low, high, size);
+		
+		quick_req(array, low, part - 1, size);
+		quick_req(array,part + 1, high, size);
+	}
 }
-
-int partition(int *array, int low, int high)
+/**
+ * partition_arr - partitions the array.
+ * @array: arra
+ * @low: lower bound
+ * @high: higher bound
+ * @size: size of array
+ *
+ * Return: sorted index f pivot
+ */
+int partition_arr(int *array, int low, int high, size_t size)
 {
-   int i = low;
-   int j;
-   int pivot = array[high];
+	int i = low;
+	int j, temp, pivot;
 
-   for (j = low; j < high; j++)
-   {
-       
-       if (pivot >= array[j])
-       {
-        swap(&array[i], &array[j]);
-        print_array(array, high + 1);
-        i++;
-       }
-      
-   }
-   swap(&array[i], &array[high]);
-   
-   return i;
+	pivot = array[high];
+	
+	for (j = low; j < high; j++)
+	{
+		if (pivot >= array[j])
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+			i++;
+		}
+	}
+	if (i != high)
+	{
+		temp = array[high];
+		array[high] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
+	return(i);
 }
 
-void swap(int *a, int *b)
-{
-	int temp = *a;
-
-	*a = *b;
-	*b = temp;
-}
